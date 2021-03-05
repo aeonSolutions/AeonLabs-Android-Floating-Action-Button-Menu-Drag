47de1a381dc411d1a48122e4b90e660676b20fc0
Miguel Silva <mtpsilva@gmail.com>
2021-03-05 12:21:09 +0100

    update

--- app/src/main/java/com/example/willy/pusblishlibrary/MainActivity.java
+++ app/src/main/java/com/example/willy/pusblishlibrary/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.willy.pusblishlibrary;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import aeonlabs.solutions.common.layout.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
